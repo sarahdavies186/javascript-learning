5eff2e01380bca63e9291a844edfcbe662c5a558
sarah <sarahdavies186@gmail.com>
2023-06-17 14:58:28 +0100

    switch and ternary operator

--- js-fundamentals/script.js
+++ js-fundamentals/script.js
@@ -61,13 +61,68 @@ if (population > 33) {
 
 console.log(5 + 6 + "4" + 9 - 4 - 2);
 
-const numNeighbours = Number(
-  prompt("How many neighbour countries does your country have?")
-);
-if (numNeighbours === 1) {
-  console.log("only 1 border");
-} else if (numNeighbours > 1) {
-  console.log("More than 1 border");
+// const numNeighbours = Number(
+//   prompt("How many neighbour countries does your country have?")
+// );
+// if (numNeighbours === 1) {
+//   console.log("only 1 border");
+// } else if (numNeighbours > 1) {
+//   console.log("More than 1 border");
+// } else {
+//   console.log("no borders");
+// }
+
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+
+if (scoreDolphins > scoreKoalas) {
+  console.log("Dolphins win the trophy");
+} else if (scoreKoals > scoreDolphins) {
+  console.log("Koalas win the trophy");
 } else {
-  console.log("no borders");
+  console.log("Both win the trophy");
 }
+
+if (language === "English" && population > 50 && !isIsland) {
+  console.log(`You should live in ${country}`);
+} else {
+  console.log(`${country} does not meet your criteria`);
+}
+
+switch (language) {
+  case "Chinese" || "Mandarin":
+    console.log("MOST number of native speakers!");
+    break;
+  case "Spanish":
+    console.log("2nd place");
+    break;
+  case "English":
+    console.log("3rd place");
+    break;
+  default:
+    console.log("great language");
+}
+
+console.log(
+  `${country}'s population is ${population >= 33 ? "above" : "below"} average`
+);
+
+// Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+// Your tasks:
+
+// Calculate the tip, depending on the bill value. Create a variable called tip for this. It's not allowed to use an if...else statement (if it's easier for you, you can start with an if...else statement, and then try to convert it to a ternary operator).
+
+// Print a string to the console containing the bill value, the tip, and the final value (bill + tip).
+
+// Example: The bill was 275, the tip was 41.25, and the total value 316.25.
+
+// Note: Use the values of the bill and tip variables to construct this string. Don't hard-code them 🙂
+
+// TEST DATA: Test with different bill values: 275, 40, and 430
+
+const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(
+  `The bill was ${bill}, the tip was ${tip} and the total value ${bill + tip}`
+);
