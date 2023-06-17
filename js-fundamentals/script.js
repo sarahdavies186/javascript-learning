f7fa9a9abd4579f490753e6b62fa09b4b0aec9e7
sarah <sarahdavies186@gmail.com>
2023-06-17 12:32:23 +0100

    variables and data types

---
+++ js-fundamentals/script.js
@@ -0,0 +1,13 @@
+const country = "England";
+const continent = "Europe";
+let population = "68 million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const isIsland = true;
+const language = "English";
+
+console.log(typeof true);
+console.log(typeof language);
