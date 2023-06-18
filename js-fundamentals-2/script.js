a6bec5106cfbbe9aaf309d70babcbabff47a882a
sarah <sarahdavies186@gmail.com>
2023-06-18 13:53:08 +0100

    arrays and objects

--- js-fundamentals-2/script.js
+++ js-fundamentals-2/script.js
@@ -46,28 +46,6 @@ const describePopulation = function (country, population) {
 
 console.log(describePopulation("china", 1441));
 
-// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
-
-// Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
-
-// A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
-
-// Your tasks:
-
-// Create an arrow function calcAverage to calculate the average of 3 scores. This function should have three parameters and return a single number (the average score).
-
-// Create two new variables — scoreDolphins and scoreKoalas, and assign the value returned from the calcAverage function to them (you will need to call this function, and pass scores as arguments).
-
-// Create a function checkWinner that takes the average score of each team as parameters (avgDolphins and avgKoalas), and then logs the winner to the console, together with the victory points, according to the rule above. Example: Koalas win (30 vs. 13) (use avgDolphins and avgKoalas instead of hard-coded values).
-
-// Use the checkWinner function to determine the winner for both DATA 1 and DATA 2.
-
-// Ignore draws this time. Instead, log No team wins... to the console if there is no winner.
-
-// TEST DATA 1: Dolphins scored 44, 23, and 71. Koalas scored 65, 54, and 49.
-
-// TEST DATA 2: Dolphins scored 85, 54, and 41. Koalas scored 23, 34, and 27.
-
 const calcAverage = (a, b, c) => (a + b + c) / 3;
 
 let scoreDolphins = calcAverage(44, 23, 71);
@@ -85,6 +63,8 @@ const checkWinner = function (avgDolphins, avgKoalas) {
 
 checkWinner(scoreDolphins, scoreKoalas);
 
+//arrays
+
 const populations = [10, 20, 30, 40];
 console.log(populations.length === 4);
 const percentages = [
@@ -96,9 +76,9 @@ const percentages = [
 console.log(percentages);
 
 const neighbours = ["Portugal", "France", "Andorra"];
-neighbours.push("Utopia");
+neighbours.push("Utopia"); // add to the end of array, unshift for beginning
 console.log(neighbours);
-neighbours.pop();
+neighbours.pop(); // remove from the end, shift for beginning
 console.log(neighbours);
 
 if (!neighbours.includes("Germany")) {
@@ -107,3 +87,48 @@ if (!neighbours.includes("Germany")) {
 
 neighbours[0] = "portugal";
 console.log(neighbours);
+
+// Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+// Your tasks:
+
+// Write a function calcTip that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from the first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+
+// And now let's use arrays! So, create an array called bills containing the test data below.
+
+// Create an array called tips containing the tip value for each bill, calculated from the function you created before.
+
+// BONUS: Create an array total containing the total values, so the bill + tip.
+
+// TEST DATA: 125, 555, and 44.
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(totals);
+
+//objects
+
+const myCountry = {
+  country: "england",
+  capital: "london",
+  language: "english",
+  population: 68,
+};
+
+console.log(myCountry);
+
+console.log(
+  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, and a capital called ${myCountry.capital}`
+);
+
+myCountry.population += 2;
+console.log(myCountry);
+
+myCountry["population"] -= 2;
+console.log(myCountry);
