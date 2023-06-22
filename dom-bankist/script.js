2ec59e5904d778da5938eeca8f1cd4ecee8415d5
sarah <sarahdavies186@gmail.com>
2023-06-22 11:31:08 +0100

    dom manipulation exercises

--- dom-bankist/script.js
+++ dom-bankist/script.js
@@ -66,9 +66,7 @@ navLinks.addEventListener("click", function (e) {
   e.preventDefault();
 
   if (e.target.classList.contains("nav__link")) {
-    console.log(e.target);
     const id = e.target.getAttribute("href");
-    console.log(id);
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -15,3 +15,32 @@ function showMessage() {
 showMessage();
 
 body.append("hello world!");
+
+//exercise 1
+const fruitList = ["apple", "banana", "tomato"];
+
+const ulElement = document.querySelector("ul");
+
+fruitList.forEach((element) => {
+  const listEl = document.createElement("li");
+  listEl.textContent = element;
+  ulElement.append(listEl);
+});
+
+//exercise 2
+const imageSrc = "https://crocoder.dev/icon.png";
+const divEl = document.querySelector("div");
+const imgEl = document.createElement("img");
+divEl.style.maxWidth = "10%";
+imgEl.setAttribute("src", imageSrc);
+imgEl.style.height = "auto";
+imgEl.style.maxWidth = "100%";
+divEl.append(imgEl);
+
+//exercise 3
+const firstList = document.querySelectorAll("ul > li:first-child");
+const lastList = document.querySelectorAll("ul > li:last-child");
+
+firstList.forEach((firstLi) => (firstLi.textContent = "first"));
+
+lastList.forEach((lastLi) => (lastLi.textContent = "last"));
