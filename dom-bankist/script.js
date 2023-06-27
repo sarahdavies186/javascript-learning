2969cb16848f216dc77348b667aa86ffadb6c94b
sarah <sarahdavies186@gmail.com>
2023-06-27 10:07:48 +0100

    functionality for slider

--- dom-bankist/script.js
+++ dom-bankist/script.js
@@ -186,7 +186,7 @@ const loadImg = function (entries, observer) {
     entry.target.classList.remove("lazy-img");
   });
 
-  observer.unobserve(entry);
+  observer.unobserve(entry.target);
 };
 
 const imgObserver = new IntersectionObserver(loadImg, {
@@ -196,3 +196,40 @@ const imgObserver = new IntersectionObserver(loadImg, {
 });
 
 imgTargets.forEach((image) => imgObserver.observe(image));
+
+//slider
+const slides = document.querySelectorAll(".slide");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+goToSlide(0);
+
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", prevSlide);
